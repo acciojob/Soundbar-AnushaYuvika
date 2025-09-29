4e8bda597ce6031666c600ca2bcab824d4b896b0
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-09-29 20:33:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+let sound = ["applause", "boo", "gasp", "tada", "victory", "wrong", "stop"];
+document.querySelectorAll(".btn").forEach((button) => {
+	button.addEventListener("click", () => {
+		stopSound();
+
+		const sound = button.textContent.trim();
+	    const audio = document.createElement("audio");
+	    audio.src = `./sounds/${sound}.mp3`; 
+	    audio.play();
+	    audio.id = "current-audio";
+	    document.body.appendChild(audio);
+	})
+})
+
+document.querySelector(".stop").addEventListener("click", () => {
+  stopSounds();
+});
+
+function stopSounds() {
+  const current = document.getElementById("current-audio");
+  if (current) {
+    current.pause();
+    current.remove();
+  }
+}
