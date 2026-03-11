7eae280e7d305b534310a218abd6bf054d02726c
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2026-03-11 18:10:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    const sound = btn.textContent.trim();
+    const audio = new Audio("sounds/" + sound + ".mp3");
+    audio.play();
+  });
+});
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  const audio = new Audio("sounds/applause.mp3");
+  audio.play();
+});
